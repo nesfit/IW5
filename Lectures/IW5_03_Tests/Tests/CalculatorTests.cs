c2aedef99411c7d856bb381b25c3cb3998d7d1f3
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2017-02-06 13:26:32 +0100

    03 tests - Fixed calculator tests and added test to show thrown exception

--- Lectures/IW5_03_Tests/Tests/CalculatorTests.cs
+++ Lectures/IW5_03_Tests/Tests/CalculatorTests.cs
@@ -8,24 +8,31 @@ namespace Tests
     public class CalculatorTests
     {
         [TestMethod]
-        public void TwoFive_Add_Returns7()
+        public void TwoAddFive_Calculate_Returns7()
         {
             int result = Calculator.Calculate(Operations.Add, 2, 5);
             Assert.AreEqual(7, result, " All operations need to follow mathematic rules.");
         }
 
         [TestMethod]
-        public void TwoMaximum_Add_Returns7()
+        public void TwoAddMaximum_Calculate_Returns7()
         {
             int result = Calculator.Calculate(Operations.Add, 2, int.MaxValue);
             Assert.AreEqual(-2147483647, result, " All operations need to follow mathematic rules.");
         }
 
         [TestMethod]
-        public void FiveOne_Add_Returns4()
+        public void FiveSubOne_Calculate_Returns4()
         {
             int result = Calculator.Calculate(Operations.Sub, 5, 1);
             Assert.AreEqual(4, result, " All operations need to follow mathematic rules.");
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void UnknownCommand_Calculate_Throws()
+        {
+            Calculator.Calculate((Operations)int.MaxValue, 5, 1);
         }
     }
 }
