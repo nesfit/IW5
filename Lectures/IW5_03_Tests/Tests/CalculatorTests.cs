cf026099460616a82637bf123ab45bfda7cf932d
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2017-02-06 13:22:34 +0100

    03 tests - Added simple unit tests for calculator

---
+++ Lectures/IW5_03_Tests/Tests/CalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using IW5_03_Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TwoFive_Add_Returns7()
+        {
+            int result = Calculator.Calculate(Operations.Add, 2, 5);
+            Assert.AreEqual(7, result, " All operations need to follow mathematic rules.");
+        }
+
+        [TestMethod]
+        public void TwoMaximum_Add_Returns7()
+        {
+            int result = Calculator.Calculate(Operations.Add, 2, int.MaxValue);
+            Assert.AreEqual(-2147483647, result, " All operations need to follow mathematic rules.");
+        }
+
+        [TestMethod]
+        public void FiveOne_Add_Returns4()
+        {
+            int result = Calculator.Calculate(Operations.Sub, 5, 1);
+            Assert.AreEqual(4, result, " All operations need to follow mathematic rules.");
+        }
+    }
+}
