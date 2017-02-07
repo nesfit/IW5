9f9d5501e5678b5a853e4486bd15d034ceee86e9
Jan Pluskal <ipluskal@fit.vutbr.cz>
2017-02-07 21:19:34 +0100

    Creat CalculatorConsole template.

--- Labs/IW5-Excercise-01/CalculatorConsole/Program.cs
+++ Labs/IW5-Excercise-01/CalculatorConsole/Program.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CalculatorConsole
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
