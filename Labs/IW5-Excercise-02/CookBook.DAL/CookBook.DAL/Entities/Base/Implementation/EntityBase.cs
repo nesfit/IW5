44a68bf043010209df991dc7a142526e98a4ae90
tjasek <tibor.jasek@pa-ats.com>
2017-03-05 22:48:20 +0100

    added interface for entities

--- Labs/IW5-Excercise-02/CookBook.DAL/CookBook.DAL/Entities/Base/Implementation/EntityBase.cs
+++ Labs/IW5-Excercise-02/CookBook.DAL/CookBook.DAL/Entities/Base/Implementation/EntityBase.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using CookBook.DAL.Entities.Base.Interface;
+using System;
 
 namespace CookBook.DAL.Entities.Base.Implementation
 {
-    public abstract class EntityBase
+    public abstract class EntityBase : IEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
     }
