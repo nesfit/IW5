1c3cb504f02415cdd7cf7dd623492855b7c72a2f
tjasek <tibor.jasek@pa-ats.com>
2017-03-05 23:55:01 +0100

    enabled EF migrations for DAL project

---
+++ Labs/IW5-Excercise-02/CookBook.DAL/CookBook.DAL/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+namespace CookBook.DAL.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CookBookDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CookBookDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
