b0683f292a4b6f273b03782c9fcbe07fd37e0009
tjasek <tibor.jasek@pa-ats.com>
2017-03-06 09:52:57 +0100

    added properties to IngredientEntity + migration

--- Labs/IW5-Excercise-02/CookBook.DAL/CookBook.DAL/Entities/IngredientEntity.cs
+++ Labs/IW5-Excercise-02/CookBook.DAL/CookBook.DAL/Entities/IngredientEntity.cs
@@ -1,9 +1,28 @@
 ﻿using CookBook.DAL.Entities.Base.Implementation;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace CookBook.DAL.Entities
 {
     public class IngredientEntity : EntityBase
     {
+        [Required]
         public string Name { get; set; }
+        public string Description { get; set; }
+        public Int32 Argb
+        {
+            get
+            {
+                return Color.ToArgb();
+            }
+            set
+            {
+                Color = Color.FromArgb(value);
+            }
+        }
+        [NotMapped]
+        public Color Color { get; set; }
     }
 }
