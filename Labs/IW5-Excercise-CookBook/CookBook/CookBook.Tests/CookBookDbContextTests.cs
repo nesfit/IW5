0804de806f29c3a0ab4f4282b1699a0f38b3fee8
Martin Dybal <martin@dybal.it>
2017-03-08 03:12:14 +0100

    Add test project

---
+++ Labs/IW5-Excercise-CookBook/CookBook/CookBook.Tests/CookBookDbContextTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookBook.DAL;
+using CookBook.DAL.Migrations;
+using Xunit;
+
+namespace CookBook.Tests
+{
+    public class CookBookDbContextTests
+    {
+        [Fact]
+        public void DbConnectionTest()
+        {
+            using (var cookBookDbContext = new CookBookDbContext())
+            {
+                cookBookDbContext.Recipes.Any();
+            }
+        }
+    }
+}
