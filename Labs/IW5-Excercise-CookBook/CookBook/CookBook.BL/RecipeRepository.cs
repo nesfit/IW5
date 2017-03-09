357ce7c30265c1dc7d9338ffa1e521f28ad2d8a2
Martin Dybal <martin@dybal.it>
2017-03-09 10:56:11 +0100

    Add BL skeleton

---
+++ Labs/IW5-Excercise-CookBook/CookBook/CookBook.BL/RecipeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookBook.DAL;
+using CookBook.DAL.Entities;
+
+namespace CookBook.BL
+{
+    public class RecipeRepository
+    {
+        public RecipeEntity FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
