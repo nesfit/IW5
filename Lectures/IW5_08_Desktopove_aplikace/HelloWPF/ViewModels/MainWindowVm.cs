c698e03c2f456b94117ec49b809e86f5ad89fb52
Jan Pluskal <ipluskal@fit.vutbr.cz>
2017-03-20 10:44:38 +0100

    Lecture08 - Create MainWindow view model enclosing navigation menu VM.

---
+++ Lectures/IW5_08_Desktopove_aplikace/HelloWPF/ViewModels/MainWindowVm.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWPF.ViewModels
+{
+    public class MainWindowVm
+    {
+        public MainWindowVm()
+        {
+            NavigationMenu = new MenuVm();
+        }
+
+        public MenuVm NavigationMenu { get; set; }
+    }
+}
