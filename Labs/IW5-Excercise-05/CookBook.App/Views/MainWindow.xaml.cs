a2b50f461824f0a44d4c0cab7315d8d0c2b2867d
Adam Jez <adamjez@outlook.cz>
2017-04-12 20:53:43 +0200

    Removed unnecessary creation of viewmodel

--- Labs/IW5-Excercise-05/CookBook.App/Views/MainWindow.xaml.cs
+++ Labs/IW5-Excercise-05/CookBook.App/Views/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ namespace CookBook.App.Views
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel(new Messenger());
         }
     }
 }
