1f631dfae755bd1efac148b240b288e2aa23d30a
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-02-11 18:44:55 +0100

    l2 dev

---
+++ Lectures/Lecture_02/EFSimpleExample/Blog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EFSimpleExample
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+}
---
+++ Lectures/Lecture_02/EFSimpleExample/BloggingContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace EFSimpleExample
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext():base("EFSimpleExample")
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
