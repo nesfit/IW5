4b38efd08f2c1a1ad367c5c9384db1e6aadb474b
tjasek <tibor.jasek@pa-ats.com>
2018-02-21 09:48:19 +0100

    added solution for IW5-Exercise-02-after added EF support added IDbSets for existing entities

---
+++ Labs/IW5-Exercise-02-after/CookBook.DAL/Entities/IngredientEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CookBook.DAL.Entities
+{
+    public class IngredientEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+    }
+}
