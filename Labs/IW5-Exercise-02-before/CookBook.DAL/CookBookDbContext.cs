4e354758816b692c4ca5201751c6ee9cb26f4f9a
tjasek <tibor.jasek@pa-ats.com>
2018-02-21 09:42:14 +0100

    renamed solution added Entities and CookBookDbContext class - without EF, EF overrides commented out

---
+++ Labs/IW5-Exercise-02-before/CookBook.DAL/CookBookDbContext.cs
@@ -0,0 +1,32 @@
+﻿namespace CookBook.DAL
+{
+    public class CookBookDbContext
+    {
+        public CookBookDbContext()
+        {
+        }
+
+        //public override int SaveChanges()
+        //{
+        //    //FixAttachedEntities();
+        //    return base.SaveChanges();
+        //}
+
+        //private void FixAttachedEntities()
+        //{
+        //    // We don't want to reInsert ingredients with existing ID so we need to attach them to db context
+        //    foreach (var entry in ChangeTracker.Entries())
+        //    {
+        //        if (entry.Entity is IEntity)
+        //        {
+        //            var intEntity = (IEntity)entry.Entity;
+        //            if (intEntity.Id != Guid.Empty && (entry.State == EntityState.Detached || entry.State == EntityState.Added))
+        //            {
+        //                Set(intEntity.GetType()).Attach(intEntity);
+        //                Entry(intEntity).State = EntityState.Modified;
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Labs/IW5-Exercise-02-before/CookBook.DAL/Entities/RecipeEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CookBook.DAL.Entities
+{
+    public class RecipeEntity
+    {
+        public Guid RecipeEntityId { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public FoodType Type { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
