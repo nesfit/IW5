57c7f85696b20008223039f15e03b1ae06091225
jirkapok <j.pok@post.cz>
2018-03-05 19:42:20 +0100

    Added lecture 5

---
+++ Lectures/Lecture_05/Tests/Tests/DatabaseStoreTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using IW5_Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DatabaseStoreTests
+    {
+        // the only think you need to do to fix this test is to update the connection string in app.config
+        // see also www.connectionstrings.com
+        [TestMethod]
+        public void ValidName_Add_AddsToDatabase()
+        {
+            // GIVEN
+            const string ExpectedName = "ExpectedName";
+            var store = new DatabaseStore();
+
+            // WHEN
+            store.Add(ExpectedName);
+
+            // THEN
+            var contains = store.GetTasks().Any(t => t.Name == ExpectedName);
+            Assert.IsTrue(contains, "The added item should be available to others in the database.");
+        }
+    }
+}
