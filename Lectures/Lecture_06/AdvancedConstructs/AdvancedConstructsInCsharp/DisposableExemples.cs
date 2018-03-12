85c1d7bc13d040e170f1dcffae1bf265a250246f
jirkapok <j.pok@post.cz>
2018-03-12 21:11:31 +0100

    Enhanced Disposable examples

--- Lectures/Lecture_06/AdvancedConstructs/AdvancedConstructsInCsharp/DisposableExemples.cs
+++ Lectures/Lecture_06/AdvancedConstructs/AdvancedConstructsInCsharp/DisposableExemples.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdvancedConstructsInCsharp
@@ -6,25 +7,29 @@ namespace AdvancedConstructsInCsharp
     [TestClass]
     public class DisposableExemples
     {
-
+        [TestMethod]
         public void UsingWrapsDisposable()
         {
+            // only IDisposable can be used in using block
+            // this is expanded by compiler to try-finally
             using (var toDispose = new DisposableClass())
             {
                 toDispose.Run();
-            }
+            }          
         }
     }
 
     //https://msdn.microsoft.com/en-us/library/system.idisposable(v=vs.110).aspx
     class DisposableClass : IDisposable
     {
+        // if you need to release the resource see, if it implements IDisposable
+        FileStream f;
         public void Run()
         {
         }
 
         // Flag: Has Dispose already been called?
-        bool disposed = false;
+        private bool disposed = false;
 
         // Public implementation of Dispose pattern callable by consumers.
         public void Dispose()
@@ -46,6 +51,7 @@ namespace AdvancedConstructsInCsharp
             if (disposing)
             {
                 // Free any other managed objects here.
+                // e.g. f.Dispose();
             }
 
             // Free any unmanaged objects here.
