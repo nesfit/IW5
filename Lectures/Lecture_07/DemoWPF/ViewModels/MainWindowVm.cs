0d363c8e854c6efd0a21d9d43cc59dee77328369
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-03-19 12:41:02 +0100

    Add Lecture07

---
+++ Lectures/Lecture_07/DemoWPF/ViewModels/MainWindowVm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using HelloWPF.Annotations;
+
+namespace HelloWPF.ViewModels
+{
+    public class MainWindowVm:INotifyPropertyChanged
+    {
+        public MainWindowVm()
+        {
+            NavigationMenu = new MenuVm();
+        }
+
+        public MenuVm NavigationMenu { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Lectures/Lecture_07/README.md
@@ -0,0 +1,3 @@
+# Lecture 07
+* [Slides - master](https://gitpitch.com/FitIW/5?grs=github&p=Lectures%2FLecture_07#/)
+* [Slides - develop](https://gitpitch.com/FitIW/5/Lecture07?p=Lectures/Lecture_07#/)
