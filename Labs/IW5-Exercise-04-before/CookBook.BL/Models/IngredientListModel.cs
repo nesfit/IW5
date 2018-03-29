510b06efe1e2ea8569a1fb57afcf7b058efbd2f7
Martin Dybal <martin@dybal.it>
2018-03-29 06:38:49 +0200

    Add not used models

---
+++ Labs/IW5-Exercise-04-before/CookBook.BL/Models/IngredientListModel.cs
@@ -0,0 +1,12 @@
+﻿using CookBook.DAL.Entities;
+
+namespace CookBook.BL.Models
+{
+    public class IngredientListModel
+    {
+        public double Amount { get; set; }
+        public Unit Unit { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
