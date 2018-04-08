e1a197e744f0b26805827620ed1914a363797b1f
jirkapok <j.pok@post.cz>
2018-04-09 00:06:09 +0200

    Added lecture 10 - web aps

---
+++ Lectures/Lecture_10/IW5_12_WebApps/App_Start/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace IW5_12_WebApps
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ Lectures/Lecture_10/IW5_12_WebApps/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+// Tel owin what to do during startup
+[assembly: OwinStartupAttribute(typeof(IW5_12_WebApps.Startup))]
+
+namespace IW5_12_WebApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
