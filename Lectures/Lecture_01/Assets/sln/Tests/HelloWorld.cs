6f90abb2a14a4c2bbb7d8407d58655a77b6c6ec0
Jan Pluskal <ipluskal@fit.vutbr.cz>
2019-02-04 08:13:40 +0100

    L1

--- Lectures/Lecture_01/Assets/sln/Tests/HelloWorld.cs
+++ Lectures/Lecture_01/Assets/sln/Tests/HelloWorld.cs
@@ -1,15 +1,15 @@
-using System;                                            // Importing namespace
+using System;                                                  // Importing namespace
 
-namespace Tests                                          // Namespace declaration
+namespace Tests                                                // Namespace declaration
 {
-    public class HelloWorld                              // Class declaration
+    public class HelloWorld                                    // Class declaration
     {
-        private static void Main(string[] args)          // Method declaration
+        private static void Main(string[] args)                // Method declaration
         {
             Console.WriteLine("Hello World!");           // Statement 1
 
             Console.WriteLine("Press any key to exit."); // Statement 2
-            Console.ReadKey();                           // Statement 3
+            Console.ReadKey();                                // Statement 3
         }
     }
 }
