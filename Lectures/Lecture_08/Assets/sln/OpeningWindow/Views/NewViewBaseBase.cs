eb0d8367375bcfab2f3365bb8e547583fd521de5
Jan Pluskal <ipluskal@fit.vutbr.cz>
2019-03-25 06:44:28 +0100

    L8

---
+++ Lectures/Lecture_08/Assets/sln/OpeningWindow/Views/NewViewBaseBase.cs
@@ -0,0 +1,12 @@
+﻿namespace OpeningWindow.Views
+{
+    public class NewViewBaseBase : IViewBase
+    {
+        public void Show()
+        {
+            // Here can be switch for more options
+            // e.g. Depends on application settings
+            new NewView().Show();
+        }
+    }
+}
