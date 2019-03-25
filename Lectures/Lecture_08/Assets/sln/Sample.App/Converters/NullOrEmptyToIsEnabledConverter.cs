013898227bad80fe8642396b2814eeb7966ce479
Jan Pluskal <ipluskal@fit.vutbr.cz>
2019-03-25 09:23:37 +0100

    L8

--- Lectures/Lecture_08/Assets/sln/Sample.App/Converters/NullOrEmptyToIsEnabledConverter.cs
+++ Lectures/Lecture_08/Assets/sln/Sample.App/Converters/NullOrEmptyToIsEnabledConverter.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
-
 namespace Sample.App.Converters
 {
     public class NullOrEmptyToIsEnabledConverter : IValueConverter
