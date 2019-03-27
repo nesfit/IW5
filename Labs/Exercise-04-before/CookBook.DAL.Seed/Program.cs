4f174b399c42cdd9239f619d9306559de6bf4fa2
Mrnda <michal.mrnustik@outlook.com>
2019-03-27 16:13:52 +0100

    Fix seeding of data causing SqlExceptions

--- Labs/Exercise-04-before/CookBook.DAL.Seed/Program.cs
+++ Labs/Exercise-04-before/CookBook.DAL.Seed/Program.cs
@@ -9,49 +9,56 @@ namespace CookBook.DAL.Seed
     {
         static void Main(string[] args)
         {
-            using(var dbContext = CreateDbContext())
+            using (var dbContext = CreateDbContext())
             {
-                var darkChocolate = new IngredientEntity
-                {
-                    Id = new Guid("5abdfee1-c970-4afd-aff8-aa3cfef8b1ac"),
-                    Name = "Tmavá čokoláda",
-                    Description = "Tmavá čokoláda s 80% kakaa.",
-                };
-                dbContext.Ingredients.Add(darkChocolate);
+                ClearDatabase(dbContext);
+                SeedData(dbContext);
+            }
+        }
 
-                var wholeMilk = new IngredientEntity
-                {
-                    Id = new Guid("83041385-cb60-401b-bf11-cc5ffb8bc570"),
-                    Name = "Plnotučné mlieko",
-                    Description = "Plnotučné mlieko so 4% tuku.",
-                };
-                dbContext.Ingredients.Add(wholeMilk);
+        private static void SeedData(CookBookDbContext dbContext)
+        {
+            var darkChocolate = new IngredientEntity
+            {
+                Id = new Guid("5abdfee1-c970-4afd-aff8-aa3cfef8b1ac"),
+                Name = "Tmavá čokoláda",
+                Description = "Tmavá čokoláda s 80% kakaa.",
+            };
+            dbContext.Ingredients.Add(darkChocolate);
 
-                var almondFlour = new IngredientEntity
-                {
-                    Id = new Guid("cb181669-4e02-449f-bf02-ab6020dfecb4"),
-                    Name = "Mandlová múka",
-                    Description = "Najemno umletá mandlová múka",
-                };
-                dbContext.Ingredients.Add(almondFlour);
+            var wholeMilk = new IngredientEntity
+            {
+                Id = new Guid("83041385-cb60-401b-bf11-cc5ffb8bc570"),
+                Name = "Plnotučné mlieko",
+                Description = "Plnotučné mlieko so 4% tuku.",
+            };
+            dbContext.Ingredients.Add(wholeMilk);
 
-                var egg = new IngredientEntity
-                {
-                    Id = new Guid("012ac89a-94e3-4bc2-94b5-c9b05fc83375"),
-                    Name = "Vajíčko",
-                    Description = "Slepačie vajíčko.",
-                };
-                dbContext.Ingredients.Add(egg);
+            var almondFlour = new IngredientEntity
+            {
+                Id = new Guid("cb181669-4e02-449f-bf02-ab6020dfecb4"),
+                Name = "Mandlová múka",
+                Description = "Najemno umletá mandlová múka",
+            };
+            dbContext.Ingredients.Add(almondFlour);
 
-                dbContext.Recipes.Add(
-                    new RecipeEntity
+            var egg = new IngredientEntity
+            {
+                Id = new Guid("012ac89a-94e3-4bc2-94b5-c9b05fc83375"),
+                Name = "Vajíčko",
+                Description = "Slepačie vajíčko.",
+            };
+            dbContext.Ingredients.Add(egg);
+
+            dbContext.Recipes.Add(
+                new RecipeEntity
+                {
+                    Id = new Guid("cb8db9b3-799c-4ef2-9d85-ce32a9ffa843"),
+                    Name = "Čokoládová torta",
+                    Duration = TimeSpan.FromMinutes(30),
+                    FoodType = FoodType.Dessert,
+                    Ingredients =
                     {
-                        Id = new Guid("cb8db9b3-799c-4ef2-9d85-ce32a9ffa843"),
-                        Name = "Čokoládová torta",
-                        Duration = TimeSpan.FromMinutes(30),
-                        FoodType = FoodType.Dessert,
-                        Ingredients =
-                        {
                         new IngredientAmountEntity
                         {
                             Id = new Guid("1d2e7873-3e35-4d40-877c-a3d0d78de3c0"),
@@ -80,13 +87,19 @@ namespace CookBook.DAL.Seed
                             Unit = Unit.Spoon,
                             Ingredient = almondFlour
                         }
-                        }
-                    });
-                dbContext.SaveChanges();
-            }
+                    }
+                });
+            dbContext.SaveChanges();
         }
 
-        static CookBookDbContext CreateDbContext()
+        private static void ClearDatabase(CookBookDbContext dbContext)
+        {
+            dbContext.RemoveRange(dbContext.Recipes);
+            dbContext.RemoveRange(dbContext.Ingredients);
+            dbContext.SaveChanges();
+        }
+
+        private static CookBookDbContext CreateDbContext()
         {
             var optionsBuilder = new DbContextOptionsBuilder<CookBookDbContext>();
             optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog = TasksDB;MultipleActiveResultSets = True;Integrated Security = True; ");
