5f601b52c79ad8dcf81d37ad5a2e7a3b5798dcec
jasho <roman.jasek@hotmail.com>
2019-04-04 22:09:39 +0200

    Added code for exercise 05

---
+++ Labs/Exercise-05-after/CookBook.BL/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CookBook.BL.Models
+{
+    public abstract class BaseModel : INotifyPropertyChanged
+    {
+        public Guid Id { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
