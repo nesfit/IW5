b289d5d9dda758e5e9943ab290f2a0ff758522fb
Michal Mrnustik <michal.mrnustik@outlook.com>
2020-02-23 17:28:44 +0100

    Add Logging controller

---
+++ Labs/Exercise-04-after/sln/CookBook.Api/Controllers/v3/LoggingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
+
+namespace CookBook.Api.Controllers.v3
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [ApiVersion("3.0")]
+    public class LoggingController : ControllerBase
+    {
+        private const string ApiOperationBaseName = "Logging";
+        private readonly ILogger<LoggingController> logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [OpenApiOperation(ApiOperationBaseName + nameof(LogSomething))]
+        public void LogSomething()
+        {
+            logger.LogInformation("Something");
+        }
+    }
+}
