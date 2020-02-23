77cb93b46dc89953b8f6b6314562d6f224b0cf28
Michal Mrnustik <michal.mrnustik@outlook.com>
2020-02-23 17:14:36 +0100

    Add after for lab04 after first step

---
+++ Labs/Exercise-04-after/sln/CookBook.DAL/Installers/DALInstaller.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CookBook.Common.Installers;
+using CookBook.DAL.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CookBook.DAL.Installers
+{
+    public class DALInstaller : IInstaller
+    {
+        public void Install(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<Storage>();
+
+            serviceCollection.Scan(selector =>
+                selector.FromCallingAssembly()
+                .AddClasses(classes => classes.AssignableTo(typeof(IAppRepository<>)))
+                .AsSelfWithInterfaces()
+                .WithTransientLifetime()
+            );
+        }
+    }
+}
