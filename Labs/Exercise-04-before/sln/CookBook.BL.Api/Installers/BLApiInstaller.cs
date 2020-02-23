262f0faabb76e55f14300ff31a34bc3e5a851a7a
Michal Mrnustik <michal.mrnustik@outlook.com>
2020-02-23 16:58:05 +0100

    Add before exercise variant

---
+++ Labs/Exercise-04-before/sln/CookBook.BL.Api/Installers/BLApiInstaller.cs
@@ -0,0 +1,15 @@
+﻿using CookBook.BL.Api.Facades;
+using CookBook.Common.Installers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CookBook.BL.Api.Installers
+{
+    public class BLApiInstaller : IInstaller
+    {
+        public void Install(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IngredientFacade>();
+            serviceCollection.AddTransient<RecipeFacade>();
+        }
+    }
+}
---
+++ Labs/Exercise-04-before/sln/CookBook.Common/Enums/Unit.cs
@@ -0,0 +1,13 @@
+﻿namespace CookBook.Common.Enums
+{
+    public enum Unit
+    {
+        Unknown = 0,
+        Kg = 1,
+        L = 2,
+        Ml = 3,
+        G = 4,
+        Pieces = 5,
+        Spoon = 6
+    }
+}
