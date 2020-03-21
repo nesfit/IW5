f8f36f87282d3628a8f965a76b745d1d794f078f
Michal Tichý <michal.tichy@riganti.cz>
2020-03-21 14:28:31 +0100

    Exercise5 - before / after (#25)
    * exercise 6 added before / after
    
    * removed files which should not be in repo
    
    * exercise 6 is now exercise 5

---
+++ Labs/Exercise-05-before/IW5_Tests/Database/TasksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace IW5_Tests.Database
+{
+    public class TasksDbContext : DbContext
+    {
+        public DbSet<UserTask> UserTasks { get; set; }
+        public TasksDbContext(DbContextOptions<TasksDbContext> contextOptions)
+            : base(contextOptions)
+        {
+        }
+    }
+}
