0e32c6a2305610f6eafd8f5a5b555c01b6da4a1e
Michal Tichý <michal.tichy@riganti.cz>
2020-03-21 12:04:18 +0100

    exercise 6 added before / after

---
+++ Labs/Exercise-06-before/IW5_Tests/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IW5_Tests
+{
+    public static class Calculator
+    {
+        public static int Calculate(Operations command, int first, int second)
+        {
+            switch (command)
+            {
+                case Operations.Add:
+                    return Add(first, second);
+                case Operations.Sub:
+                    return Sub(first, second);
+                default:
+                    throw new InvalidOperationException("The command is not supported.");
+            }
+        }
+
+        private static int Sub(int first, int second)
+        {
+            checked
+            {
+                return first - second;
+            }
+        }
+
+        private static int Add(int first, int second)
+        {
+            checked
+            {
+                return first + second;
+            }
+        }
+    }
+}
