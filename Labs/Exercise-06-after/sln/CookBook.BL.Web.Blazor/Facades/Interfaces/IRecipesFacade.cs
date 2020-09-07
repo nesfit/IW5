bc5605134586c48ad794a7f56f984ff0e0eed673
Roman Jašek <roman.jasek@riganti.cz>
2020-09-07 16:17:46 +0200

    Added page RecipeList.

---
+++ Labs/Exercise-06-after/sln/CookBook.BL.Web.Blazor/Facades/Interfaces/IRecipesFacade.cs
@@ -0,0 +1,11 @@
+﻿using CookBook.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CookBook.BL.Web.Blazor.Facades
+{
+    public interface IRecipesFacade
+    {
+        Task<IList<RecipeListModel>> RecipesGetAsync();
+    }
+}
---
+++ Labs/Exercise-06-after/sln/CookBook.BL.Web.Blazor/Facades/RecipesFacade.cs
@@ -0,0 +1,22 @@
+﻿using CookBook.BL.Web.Blazor.Api;
+using CookBook.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CookBook.BL.Web.Blazor.Facades
+{
+    public class RecipesFacade : FacadeBase, IRecipesFacade
+    {
+        private readonly IRecipeClient recipeClient;
+
+        public RecipesFacade(IRecipeClient recipeClient)
+        {
+            this.recipeClient = recipeClient;
+        }
+
+        public async Task<IList<RecipeListModel>> RecipesGetAsync()
+        {
+            return await recipeClient.RecipeGetAsync("3", "cs");
+        }
+    }
+}
