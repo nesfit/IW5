fdccb65fbf852f1200fc5b213cf1a85318cbb4c5
Michal Tichý <email@tichymichal.net>
2020-09-07 13:58:11 +0200

    Added basic ingredientForm

--- Labs/Exercise-06-after/sln/CookBook.Models/Ingredient/IngredientDetailModel.cs
+++ Labs/Exercise-06-after/sln/CookBook.Models/Ingredient/IngredientDetailModel.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CookBook.Models.Ingredient
 {
     public class IngredientDetailModel
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
     }
 }
