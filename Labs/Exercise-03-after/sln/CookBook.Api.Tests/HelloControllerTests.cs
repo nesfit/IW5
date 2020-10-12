806298f8ca7faddade502905e36943ac595f658c
Tibor Jašek <tibor.jasek@riganti.cz>
2020-10-12 18:47:29 +0200

    Replaced IW5_Lecture_03 - Local.postman_environment.json with seperate environment variables for IIS Express and Kestrel. Added "Log Something" and "Say Your Name" requests to postman collection. Added CookBook.Api.Tests project. Added HelloController test in C# and for Postman request. Changed logging providers settings for testing purposes.

---
+++ Labs/Exercise-03-after/sln/CookBook.Api.Tests/HelloControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CookBook.Api.Tests
+{
+    public class HelloControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public HelloControllerTests(WebApplicationFactory<Startup> fixture)
+        {
+            _client = fixture.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_Should_Return_My_Name()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/hello");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            responseBody.Should().Be("My name is Karel");
+
+            //response.Content.ReadAsStringAsync().Result.Should().Be("My name is Karel");
+        }
+    }
+}
--- Labs/Exercise-03-after/sln/CookBook.Api/Program.cs
+++ Labs/Exercise-03-after/sln/CookBook.Api/Program.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -21,9 +16,10 @@ namespace CookBook.Api
                 .ConfigureLogging(builder =>
                 {
                     builder.ClearProviders()
+                           .AddDebug()
                            .AddConsole()
                            .AddFilter("System", LogLevel.Debug)
-                           .SetMinimumLevel(LogLevel.Warning);
+                           .SetMinimumLevel(LogLevel.Information);
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
