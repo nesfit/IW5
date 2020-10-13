4bae80b4e250a4963caf65376a50578e32816ad2
Tibor Jašek <tibor.jasek@riganti.cz>
2020-10-13 03:10:35 +0200

    Added eShopOnWeb.sln - contains UI tests examples.

---
+++ Labs/Exercise-03-after/sln_ui_testing/eShopOnWeb-master/tests/UITests/HomePageTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using UITests.PageObjectModels;
+
+namespace UITests
+{
+    public class HomePageTests : TestsBase
+    {
+        [Test]
+        [Category("Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                var homePage = new HomePage(webDriver);
+                homePage.NavigateTo();
+            }
+        }
+
+        [Test]
+        [Category("Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                var homePage = new HomePage(webDriver);
+                homePage.NavigateTo();
+
+                webDriver.Navigate().Refresh();
+
+                homePage.EnsurePageLoaded();
+            }
+        }
+
+        [Test]
+        [Category("Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                var homePage = new HomePage(webDriver);
+                homePage.NavigateTo();
+
+                string initialToken = homePage.GenerationToken;
+
+                webDriver.Navigate().GoToUrl(LoginUrl);
+                webDriver.Navigate().Back();
+
+                homePage.EnsurePageLoaded();
+
+                string reloadedToken = homePage.GenerationToken;
+                reloadedToken.Should().NotBe(initialToken);
+            }
+        }
+
+        [Test]
+        [Category("Smoke")]
+        public void ReloadHomePageOnForward()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                var homePage = new HomePage(webDriver);
+                homePage.NavigateTo();
+
+                string initialToken = homePage.GenerationToken;
+
+                webDriver.Navigate().Back();
+                webDriver.Navigate().Forward();
+
+                homePage.EnsurePageLoaded();
+
+                string reloadedToken = homePage.GenerationToken;
+                reloadedToken.Should().NotBe(initialToken);
+            }
+        }
+    }
+}
---
+++ Labs/Exercise-03-after/sln_ui_testing/eShopOnWeb-master/tests/UITests/MyAccountTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace UITests
+{
+    public class MyAccountTests : TestsBase
+    {
+        [Test]
+        public void PhoneNumberIsEmptyByDefault()
+        {
+            using (IWebDriver webDriver = new ChromeDriver())
+            {
+                // Arrange
+                webDriver.Navigate().GoToUrl(LoginUrl);
+
+                // Act
+                IWebElement emailInput = webDriver.FindElement(By.Id("Input_Email"));
+                emailInput.SendKeys("demouser@microsoft.com");
+
+                IWebElement passwordInput = webDriver.FindElement(By.Id("Input_Password"));
+                passwordInput.SendKeys(ValidPassword);
+
+                IWebElement loginButton = webDriver.FindElement(By.TagName("button"));
+                loginButton.Click();
+
+                IWebElement identityName = webDriver.FindElement(By.ClassName("esh-identity-name"));
+
+                Actions action = new Actions(webDriver);
+                action.MoveToElement(identityName).Click(webDriver.FindElement(By.XPath("//section[@class='esh-identity-drop']//a[@href=\"/manage/my-account\"]"))).Build().Perform();
+
+                // Assert
+                IWebElement phoneNumber = webDriver.FindElement(By.Id("PhoneNumber"));
+                phoneNumber.Text.Should().BeEmpty();
+            }
+        }
+    }
+}
