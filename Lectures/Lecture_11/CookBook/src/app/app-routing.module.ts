41e85d04f9bdfec2e003c4bf1d9a352045df91eb
Erik Machacek <erik.machacek@solarwinds.com>
2020-11-27 11:03:55 +0100

    Updated routing

--- Lectures/Lecture_11/CookBook/src/app/app-routing.module.ts
+++ Lectures/Lecture_11/CookBook/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'ingredient', component: IngredientComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
