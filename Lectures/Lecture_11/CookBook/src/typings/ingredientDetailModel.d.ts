538c2e4980750f62369ea6b0017b4073f449e0f9
Erik Machacek <erik.machacek@solarwinds.com>
2020-11-27 13:09:51 +0100

    Added typings for ingredient

---
+++ Lectures/Lecture_11/CookBook/src/typings/ingredientDetailModel.d.ts
@@ -0,0 +1,7 @@
+import { Guid } from "guid-typescript";
+
+declare interface IngredientDetailModel {
+    id: Guid;
+    name: string;
+    description: string;
+}
---
+++ Lectures/Lecture_11/CookBook/src/typings/ingredientListModel.d.ts
@@ -0,0 +1,6 @@
+import { Guid } from "guid-typescript";
+
+declare interface IngredientListModel {
+    Id: Guid;
+    Name: string;
+}
