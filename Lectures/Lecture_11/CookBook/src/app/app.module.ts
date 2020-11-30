266704180d09c85d1fd0a3c04c10abcf8acaab3a
Erik Machacek <erik.machacek@solarwinds.com>
2020-11-30 18:49:32 +0100

    Added stub home

--- Lectures/Lecture_11/CookBook/src/app/app.module.ts
+++ Lectures/Lecture_11/CookBook/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IngredientComponent } from './ingredient/ingredient.component';
 import {ApiModule} from './api/api.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IngredientComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
