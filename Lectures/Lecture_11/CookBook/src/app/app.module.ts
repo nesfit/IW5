3b844639cd59f76a6663101f743db5f33c8a4b7c
Erik Machacek <erik.machacek@solarwinds.com>
2020-11-30 19:14:48 +0100

    Fixed api url

--- Lectures/Lecture_11/CookBook/src/app/app.module.ts
+++ Lectures/Lecture_11/CookBook/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ApiModule.forRoot({ rootUrl: 'https://localhost:44378//api' })
+    ApiModule.forRoot({ rootUrl: 'https://localhost:44378' })
   ],
   providers: [],
   bootstrap: [AppComponent]
