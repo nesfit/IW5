f7f84de08dc3b486e0011e4f8b5cc86000e6afab
Erik Machacek <erik.machacek@solarwinds.com>
2020-12-02 21:37:51 +0100

    Added routing to recipes

--- Lectures/Lecture_11/CookBook/src/app/app-routing.module.ts
+++ Lectures/Lecture_11/CookBook/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { IngredientComponent } from './ingredient/ingredient.component';
 import { IngredientPageComponent } from './ingredient-page/ingredient-page.component';
 import { HomeComponent } from './home/home.component';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'ingredient/:id', component: IngredientComponent },
-  { path: 'ingredients', component: IngredientPageComponent }
+  { path: 'ingredients', component: IngredientPageComponent },
+  { path: 'recipes', component: RecipePageComponent }
 ];
 
 @NgModule({
