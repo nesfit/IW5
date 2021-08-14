d5f149fd18cbe54a8b2001eb6b20daaecf1e381e
jasho <roman.jasek@hotmail.com>
2021-08-14 16:38:25 +0200

    Updated project names to match naming schema and adjusted namespaces in Web.App project

---
+++ src/CookBook/CookBook.Web.App/Pages/Recipe/RecipeListPage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CookBook.BL.Web.Facades;
+using CookBook.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CookBook.Web.App.Pages
+{
+    public partial class RecipeListPage
+    {
+        [Inject]
+        private RecipeFacade RecipeFacade { get; set; } = null!;
+
+        private ICollection<RecipeListModel> Recipes { get; set; } = new List<RecipeListModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Recipes = await RecipeFacade.GetAllAsync();
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
---
+++ src/CookBook/CookBook.Web.App/Shared/MainLayout.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using CookBook.BL.Web.Facades;
+using Microsoft.AspNetCore.Components;
+
+namespace CookBook.Web.App
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        public RecipeFacade RecipeFacade { get; set; } = null!;
+
+        [Inject]
+        public IngredientFacade IngredientFacade { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+
+        public async Task OnlineStatusChangedAsync(bool isOnline)
+        {
+            if (isOnline)
+            {
+                var dataChanged = false;
+                dataChanged = dataChanged || await IngredientFacade.SynchronizeLocalDataAsync();
+                dataChanged = dataChanged || await RecipeFacade.SynchronizeLocalDataAsync();
+
+                if (dataChanged)
+                {
+                    NavigationManager.NavigateTo(NavigationManager.Uri, true);
+                }
+            }
+        }
+    }
+}
