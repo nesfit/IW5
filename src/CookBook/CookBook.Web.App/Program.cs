cb45db2fc06886f92654632a3e66e7772be03920
jasho <roman.jasek@hotmail.com>
2021-08-15 17:25:48 +0200

    Updated Program.cs to new .NET 6 template

--- src/CookBook/CookBook.Web.App/Program.cs
+++ src/CookBook/CookBook.Web.App/Program.cs
@@ -1,34 +1,18 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using CookBook.Web.BL.Installers;
-using CookBook.Web.DAL;
 using CookBook.Web.DAL.Installers;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CookBook.Web.App
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<CookBook.Web.App.App>("app");
-            Install(builder);
-            await builder.Build().RunAsync();
-        }
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<CookBook.Web.App.App>("app");
 
-        private static void Install(WebAssemblyHostBuilder builder)
-        {
-            var apiBaseUrl = builder.Configuration.GetValue<string>("ApiBaseUrl");
+var apiBaseUrl = builder.Configuration.GetValue<string>("ApiBaseUrl");
+new WebDALInstaller().Install(builder.Services);
+new WebBLInstaller().Install(builder.Services, apiBaseUrl);
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAutoMapper(typeof(WebBLInstaller));
 
-            new WebDALInstaller().Install(builder.Services);
-            new WebBLInstaller().Install(builder.Services, apiBaseUrl);
-
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddAutoMapper(typeof(WebBLInstaller));
-        }
-    }
-}
+await builder.Build().RunAsync();
