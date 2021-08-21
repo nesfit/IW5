2a865892c9b552ef30d9f6c264ea7248730cf9c3
jasho <roman.jasek@hotmail.com>
2021-08-21 10:29:06 +0200

    Minor refactoring - removed redundant type specifications in storage seed

--- src/CookBook/CookBook.Api.DAL.Memory/Storage.cs
+++ src/CookBook/CookBook.Api.DAL.Memory/Storage.cs
@@ -9,19 +9,19 @@ namespace CookBook.Api.DAL.Memory
     {
         private readonly IList<Guid> ingredientGuids = new List<Guid>
         {
-            new Guid("df935095-8709-4040-a2bb-b6f97cb416dc"),
-            new Guid("23b3902d-7d4f-4213-9cf0-112348f56238")
+            new ("df935095-8709-4040-a2bb-b6f97cb416dc"),
+            new ("23b3902d-7d4f-4213-9cf0-112348f56238")
         };
 
         private readonly IList<Guid> ingredientAmountGuids = new List<Guid>
         {
-            new Guid("0d4fa150-ad80-4d46-a511-4c666166ec5e"),
-            new Guid("87833e66-05ba-4d6b-900b-fe5ace88dbd8")
+            new ("0d4fa150-ad80-4d46-a511-4c666166ec5e"),
+            new ("87833e66-05ba-4d6b-900b-fe5ace88dbd8")
         };
 
         private readonly IList<Guid> recipeGuids = new List<Guid>
         {
-            new Guid("fabde0cd-eefe-443f-baf6-3d96cc2cbf2e")
+            new ("fabde0cd-eefe-443f-baf6-3d96cc2cbf2e")
         };
 
         public IList<IngredientEntity> Ingredients { get; } = new List<IngredientEntity>();
