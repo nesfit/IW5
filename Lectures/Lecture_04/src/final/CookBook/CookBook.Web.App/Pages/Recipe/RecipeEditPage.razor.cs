5f76ab1f309b79e1216db18f4c58d8c79e2d5521
jasho <roman.jasek@hotmail.com>
2021-10-18 09:33:42 +0200

    Added code for "final" version of Lecture 04

---
+++ Lectures/Lecture_04/src/final/CookBook/CookBook.Web.App/Pages/Recipe/RecipeEditPage.razor.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Json;
+using CookBook.Common.Enums;
+using CookBook.Common.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CookBook.Web.App.Pages
+{
+    public partial class RecipeEditPage
+    {
+        [Inject]
+        private HttpClient httpClient { get; set; } = null!;
+
+        private RecipeDetailModel Data { get; set; } = new();
+
+        [Parameter]
+        public Guid Id { get; init; }
+
+        private IList<IngredientListModel> Ingredients { get; set; } = new List<IngredientListModel>();
+
+        private RecipeDetailIngredientModel NewIngredientModel { get; set; } = new(null, 0, Unit.Unknown, new IngredientListModel(Guid.Empty, string.Empty));
+
+        private int DurationHours
+        {
+            get => Data.Duration.Hours;
+            set => Data.Duration = new TimeSpan(value, DurationMinutes, 0);
+        }
+
+        private int DurationMinutes
+        {
+            get => Data.Duration.Minutes;
+            set => Data.Duration = new TimeSpan(DurationHours, value, 0);
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Id == Guid.Empty)
+            {
+                Data = new RecipeDetailModel
+                {
+                    IngredientAmounts = new List<RecipeDetailIngredientModel>()
+                };
+            }
+            else
+            {
+                var dataList = await httpClient.GetFromJsonAsync<List<RecipeDetailModel>>("sample-data/recipes.json");
+                if (dataList is not null)
+                {
+                    Data = dataList.Single(item => item.Id == Id);
+                }
+            }
+
+            Ingredients = await httpClient.GetFromJsonAsync<List<IngredientListModel>>("sample-data/ingredients.json")
+                ?? new List<IngredientListModel>();
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
---
+++ Lectures/Lecture_04/src/final/CookBook/CookBook.Web.App/Pages/Recipe/RecipeListPage.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using CookBook.Common.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CookBook.Web.App.Pages
+{
+    public partial class RecipeListPage
+    {
+        [Inject]
+        private HttpClient httpClient { get; set; } = null!;
+
+        private IList<RecipeListModel> Recipes { get; set; } = new List<RecipeListModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Recipes = await httpClient.GetFromJsonAsync<List<RecipeListModel>>("sample-data/recipes.json")
+                ?? new List<RecipeListModel>();
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
