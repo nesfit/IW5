efedd42bf36267bb0a95098c64bac6216e323b8c
jasho <roman.jasek@hotmail.com>
2021-11-13 11:17:23 +0100

    Added default empty MAUI project

---
+++ src/CookBook/CookBook.Maui.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Application = Microsoft.Maui.Controls.Application;
+
+namespace CookBook.Maui.App
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
