0c0fadef4f9b8b77ebab7042ed3178b9e4133187
jasho <roman.jasek@hotmail.com>
2021-11-14 13:10:14 +0100

    MAUI application: added RecipeEditView

---
+++ src/CookBook/CookBook.Maui.App/Views/Recipe/RecipeEditView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CookBook.Maui.App.Views
+{
+    public partial class RecipeEditView
+    {
+        public RecipeEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
