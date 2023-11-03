77f6fa4e09d489f2224810b0a9b83027e8f1d21c
Roman Jašek <roman.jasek@hotmail.com>
2023-11-03 14:42:04 +0100

    Added IdentityProvider project with basic configuration

---
+++ src/CookBook/CookBook.IdentityProvider/Config.cs
@@ -0,0 +1,58 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace CookBook.IdentityProvider
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ("cookbookclientaudience"),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new("cookbookapi")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new()
+                {
+                    ClientName = "CookBook Client",
+                    ClientId = "cookbookclient",
+                    AllowOfflineAccess = true,
+                    RedirectUris = new List<string>
+                    {
+                        "https://oauth.pstmn.io/v1/callback",
+                    },
+                    AllowedGrantTypes = new List<string>
+                    {
+                        GrantType.ClientCredentials,
+                        GrantType.ResourceOwnerPassword,
+                        GrantType.AuthorizationCode
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "cookbookapi"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+    }
+}
---
+++ src/CookBook/CookBook.IdentityProvider/HostingExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Serilog;
+
+namespace CookBook.IdentityProvider
+{
+    internal static class HostingExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            // uncomment if you want to add a UI
+            //builder.Services.AddRazorPages();
+
+            builder.Services.AddIdentityServer(options =>
+                {
+                    // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseSerilogRequestLogging();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add a UI
+            //app.UseStaticFiles();
+            //app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment if you want to add a UI
+            //app.UseAuthorization();
+            //app.MapRazorPages().RequireAuthorization();
+
+            return app;
+        }
+    }
+}
