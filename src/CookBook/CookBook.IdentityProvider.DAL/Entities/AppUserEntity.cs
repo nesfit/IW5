80def156cc429ad02a832f078edb7d77307ba1a9
Roman Jašek <roman.jasek@hotmail.com>
2023-11-05 16:57:33 +0100

    Added DAL project for IdentityProvider project

---
+++ src/CookBook/CookBook.IdentityProvider.DAL/Entities/AppUserEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CookBook.IdentityProvider.DAL.Entities;
+
+public class AppUserEntity : IdentityUser<Guid>
+{
+    public bool Active { get; set; }
+    public string Subject { get; set; }
+}
+
+public class AppRoleClaimEntity : IdentityRoleClaim<Guid>
+{
+}
+
+public class AppRoleEntity : IdentityRole<Guid>
+{
+}
+
+public class AppUserClaimEntity : IdentityUserClaim<Guid>
+{
+}
+
+public class AppUserLoginEntity : IdentityUserLogin<Guid>
+{
+}
+
+public class AppUserRoleEntity : IdentityUserRole<Guid>
+{
+}
+
+public class AppUserTokenEntity : IdentityUserToken<Guid>
+{
+}
