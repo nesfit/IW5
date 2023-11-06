defdb02b6b8034775dae9123e0650c3cd85ea42b
Roman Jašek <roman.jasek@hotmail.com>
2023-11-06 08:03:56 +0100

    Moved Identity entities to separate files

--- src/CookBook/CookBook.IdentityProvider.DAL/Entities/AppUserEntity.cs
+++ src/CookBook/CookBook.IdentityProvider.DAL/Entities/AppUserEntity.cs
@@ -7,27 +7,3 @@ public class AppUserEntity : IdentityUser<Guid>
     public bool Active { get; set; }
     public string Subject { get; set; }
 }
-
-public class AppRoleClaimEntity : IdentityRoleClaim<Guid>
-{
-}
-
-public class AppRoleEntity : IdentityRole<Guid>
-{
-}
-
-public class AppUserClaimEntity : IdentityUserClaim<Guid>
-{
-}
-
-public class AppUserLoginEntity : IdentityUserLogin<Guid>
-{
-}
-
-public class AppUserRoleEntity : IdentityUserRole<Guid>
-{
-}
-
-public class AppUserTokenEntity : IdentityUserToken<Guid>
-{
-}
