b368772bc6f1ceda603b4a54524a880bc56377ee
Roman Jašek <roman.jasek@hotmail.com>
2023-11-06 11:23:07 +0100

    Added support for serving UI for IdentityProvider

--- src/CookBook/CookBook.IdentityProvider.App/HostingExtensions.cs
+++ src/CookBook/CookBook.IdentityProvider.App/HostingExtensions.cs
@@ -7,8 +7,7 @@ namespace CookBook.IdentityProvider.App
     {
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
-            // uncomment if you want to add a UI
-            //builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages();
 
             builder.Services.AddIdentityServer(options =>
                 {
@@ -34,15 +33,13 @@ namespace CookBook.IdentityProvider.App
                 app.UseDeveloperExceptionPage();
             }
 
-            // uncomment if you want to add a UI
-            //app.UseStaticFiles();
-            //app.UseRouting();
+            app.UseStaticFiles();
+            app.UseRouting();
 
             app.UseIdentityServer();
 
-            // uncomment if you want to add a UI
-            //app.UseAuthorization();
-            //app.MapRazorPages().RequireAuthorization();
+            app.UseAuthorization();
+            app.MapRazorPages().RequireAuthorization();
 
             return app;
         }
