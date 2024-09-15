d63e5d4c07cd720b1e42fb9786d5ac21f29be1ac
jasho <roman.jasek@hotmail.com>
2024-09-15 12:22:24 +0200

    .NET 8 Upgrade - working version

--- src/CookBook/CookBook.Web.App/Program.cs
+++ src/CookBook/CookBook.Web.App/Program.cs
@@ -1,34 +1,30 @@
-﻿using System;
-using System.Globalization;
-using System.Net.Http;
-using AutoMapper.Internal;
+﻿using AutoMapper.Internal;
 using CookBook.Common.Extensions;
 using CookBook.Web.App;
 using CookBook.Web.BL.Extensions;
 using CookBook.Web.BL.Installers;
 using CookBook.Web.BL.Options;
 using CookBook.Web.DAL.Installers;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
-
-const string defaultCultureString = "cs";
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Configuration.AddJsonFile("appsettings.json");
 
 var apiBaseUrl = builder.Configuration.GetValue<string>("ApiBaseUrl");
 
 builder.Services.AddInstaller<WebDALInstaller>();
 builder.Services.AddInstaller<WebBLInstaller>(apiBaseUrl);
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAutoMapper(configuration =>
-    {
-        // This is a temporary fix - should be able to remove this when version 11.0.2 comes out
-        // More information here: https://github.com/AutoMapper/AutoMapper/issues/3988
-        configuration.Internal().MethodMappingEnabled = false;
-    }, typeof(WebBLInstaller));
+{
+    // This is a temporary fix - should be able to remove this when version 11.0.2 comes out
+    // More information here: https://github.com/AutoMapper/AutoMapper/issues/3988
+    configuration.Internal().MethodMappingEnabled = false;
+}, typeof(WebBLInstaller));
 builder.Services.AddLocalization();
 
 builder.Services.Configure<LocalDbOptions>(options =>
@@ -36,16 +32,7 @@ builder.Services.Configure<LocalDbOptions>(options =>
     options.IsLocalDbEnabled = bool.Parse(builder.Configuration.GetSection(nameof(LocalDbOptions))[nameof(LocalDbOptions.IsLocalDbEnabled)]);
 });
 
-var host = builder.Build();
 
-var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
-var cultureString = (await jsRuntime.InvokeAsync<string>("blazorCulture.get"))
-                    ?? defaultCultureString;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var culture = new CultureInfo(cultureString);
-await jsRuntime.InvokeVoidAsync("blazorCulture.set", cultureString);
-
-CultureInfo.DefaultThreadCurrentCulture = culture;
-CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-await host.RunAsync();
+await builder.Build().RunAsync();
