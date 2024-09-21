91e287c86be3d698b14895f9dcce9354e7491201
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2024-09-22 00:57:35 +0200

    Simplify gateway configuration

--- src/CookBook/CookBook.Hosting.Gateway/Program.cs
+++ src/CookBook/CookBook.Hosting.Gateway/Program.cs
@@ -1,82 +1,16 @@
 using CookBook.Hosting.ServiceDefaults;
-using Yarp.ReverseProxy.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
-const string apiClusterName = "api";
-const string webClusterName = "web";
-
-var routes = new RouteConfig[]
-{
-    new()
-    {
-        Order = 0,
-        ClusterId = apiClusterName,
-        RouteId = $"{apiClusterName}-api",
-        Match = new RouteMatch
-        {
-            Path = "/api/{*any}",
-        },
-    },
-    new()
-    {
-        Order = 1,
-        ClusterId = apiClusterName,
-        RouteId = $"{apiClusterName}-swagger",
-        Match = new RouteMatch
-        {
-            Path = "/swagger/{*any}",
-        },
-    },
-    new()
-    {
-        Order = 10,
-        ClusterId = webClusterName,
-        RouteId = webClusterName,
-        Match = new RouteMatch
-        {
-            Path = "{*any}"
-        },
-    },
-};
-
-DestinationConfig CreateDestinationConfig(string destination) => new() { Address = destination };
-
-if (builder.Configuration.GetRequiredSection("services:api:http").Get<string[]>() is not { Length: > 0 } apiAddresses)
-{
-    throw new ApplicationException("API service configuration is missing!");
-}
-var apiDestinations = apiAddresses
-    .Select((addr, index) => ($"api-{index}", CreateDestinationConfig(addr)))
-    .ToDictionary();
-
-if (builder.Configuration.GetRequiredSection("services:web:http").Get<string[]>() is not { Length: > 0 } webAddresses)
-{
-    throw new ApplicationException("Web service configuration is missing!");
-}
-var webDestinations = webAddresses
-    .Select((addr, index) => ($"web-{index}", CreateDestinationConfig(addr)))
-    .ToDictionary();
-
-var clusters = new ClusterConfig[]
-{
-    new()
-    {
-        ClusterId = apiClusterName,
-        Destinations = apiDestinations,
-    },
-    new()
-    {
-        ClusterId = webClusterName,
-        Destinations = webDestinations,
-    }
-};
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddReverseProxy()
-    .LoadFromMemory(routes, clusters);
+    .LoadFromConfig(builder.Configuration.GetRequiredSection("ReverseProxy"))
+    .AddServiceDiscoveryDestinationResolver();
 
 var app = builder.Build();
+app.MapDefaultEndpoints();
 
 app.UseHttpsRedirection();
 app.UseRouting();
