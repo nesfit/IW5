a19088638a7f04590dde33ec82082588eda89f37
jasho <roman.jasek@hotmail.com>
2024-10-07 08:49:44 +0200

    Reordering of lecture materials to match 2024 schedule

--- Lectures/Lecture_11/ExampleApp/Build/test.ps1
+++
@@ -1,7 +0,0 @@
-. .\Build\settings.ps1
-
-$testResults = Join-Path $global:outputDir "\TestResults\dotnetTests.trx"
-$resultsFullPath = [System.IO.Path]::GetFullPath($testResults)
-$configuration = $env:BuildConfiguration
-$testsDll = ".\Output\$configuration\Tests\net7.0\Calculator.Engine.Tests.dll"
-dotnet vstest $testsDll /logger:"trx;LogFileName=$resultsFullPath" --nologo
